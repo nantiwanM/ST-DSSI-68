161837daf730b28e0d9c7df2b61c87c67475553f
nantiwanM <nantiwan.ko.65@ubu.ac.th>
2025-08-18 14:47:27 +0700

    Update settings.py

--- CNStyleShop/settings.py
+++ CNStyleShop/settings.py
@@ -33,8 +33,8 @@ ALLOWED_HOSTS = [
     '127.0.0.1', 
     '0.0.0.0'
     ]
-# Application definition
 
+# Application definition
 INSTALLED_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
